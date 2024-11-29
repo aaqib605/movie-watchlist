c2e42519cad8f7bf721d2f7f8fe393843767c33b
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-11-29 10:14:47 +0530

    feat: make movies list & movies watchlist responsive

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -15,10 +15,31 @@ interface MovieListProps {
 
 export default function MovieList({ movies, onSelectMovie }: MovieListProps) {
   return (
-    <ul className="list list-movies">
-      {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
-      ))}
-    </ul>
+    <>
+      <ul className="list list-movies">
+        {movies.length > 0 ? (
+          movies?.map((movie) => (
+            <Movie
+              movie={movie}
+              key={movie.imdbID}
+              onSelectMovie={onSelectMovie}
+            />
+          ))
+        ) : (
+          <span
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translateX(-50%)",
+              fontSize: "1.5rem",
+            }}
+            className=""
+          >
+            Search movies to see results.
+          </span>
+        )}
+      </ul>
+    </>
   );
 }
